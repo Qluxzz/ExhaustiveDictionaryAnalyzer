76c0e6050b8297dfe1265a44b7a880c0df656be7
Qluxzz <andre.eric.andersson@gmail.com>
2026-04-08 09:42:42 +0200

    Add failing tests

---
+++ ExhaustiveDictionary.Tests/AnalyzerTests.FrozenDictionary.cs
@@ -0,0 +1,38 @@
+using Verify = Microsoft.CodeAnalysis.CSharp.Testing.CSharpCodeFixVerifier<
+    ExhaustiveDictionary.EnumDictionaryAnalyzer,
+    ExhaustiveDictionary.AddMissingEnumValuesCodeFixProvider,
+    Microsoft.CodeAnalysis.Testing.DefaultVerifier
+>;
+
+namespace ExhaustiveDictionary.Tests;
+
+public sealed partial class AnalyzerTests
+{
+    [TestMethod]
+    public async Task ReportsMissingValuesInFrozenDictionaryOnProperty()
+    {
+        var expected = Verify
+            .Diagnostic(EnumDictionaryAnalyzer.ExhaustiveRule)
+            .WithSpan(11, 38, 11, 48)
+            .WithArguments("ColorToHex", "Color.Green, Color.Blue");
+
+        await TestAnalyzer(
+            @"
+using System;
+using System.Collections.Generic;
+using System.Collections.Frozen;
+using ExhaustiveDictionary;
+
+public static class Program
+{
+    enum Color { Red, Green, Blue, };
+
+    [Exhaustive]
+    static FrozenDictionary<Color, string> ColorToHex =
+        new Dictionary<Color, string>() { Color.Red, ""#FF0000"" }.ToFrozenDictionary();
+}
+",
+            expected
+        );
+    }
+}
